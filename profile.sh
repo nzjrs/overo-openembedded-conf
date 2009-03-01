d014d241f48baf6828dae4371d16ce86a33732d7
John Stowers <john.stowers@gmail.com>
2009-03-01 14:23:25 +1300

    add note to prompt

--- profile.sh
+++ profile.sh
@@ -6,10 +6,16 @@ export BB_ENV_EXTRAWHITE="MACHINE DISTRO ANGSTROM_MODE OVEROTOP OEBRANCH USERBRA
 
 export OVEROTOP="${HOME}/overo-oe"
 export OEBRANCH="${OVEROTOP}/org.openembedded.dev"
-export USERBRANCH="${OVEROTOP}/user.collection"
+export USERBRANCH="${OVEROTOP}/local"
 
 export PATH="${OVEROTOP}/bitbake/bin:$PATH"
 export BBPATH="${OVEROTOP}/build:${USERBRANCH}:${OEBRANCH}"
+
+if [ "$PS1" ]; then
+   if [ "$BASH" ]; then
+     export PS1="\[\033[01;32m\]OE:overo\[\033[00m\] ${PS1}"
+   fi
+fi
 
 umask 0002
 
