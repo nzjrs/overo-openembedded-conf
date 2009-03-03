388dc518baaae66001ec2059b212fe69bc077ac9
John Stowers <john.stowers@gmail.com>
2009-03-04 12:49:39 +1300

    make git own remote cache

--- scripts/sync-oe-cache-to-open.sh
+++ scripts/sync-oe-cache-to-open.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 HERE="/home/john/overo-oe/sources/"
-THERE="john@open.grcnz.com:/mnt/datadisk/openembedded/"
+THERE="git@open.grcnz.com:/mnt/datadisk/openembedded/"
 
-rsync -aP $HERE $THERE
-rsync -aP $THERE $HERE
+rsync -rlptDP $HERE $THERE
+rsync -rlptDP $THERE $HERE
